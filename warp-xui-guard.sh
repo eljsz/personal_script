d6f3d63c5e25afb29d1597da3d14339091deeafd
eljsz <137702135+eljsz@users.noreply.github.com>
2023-08-12 20:41:28 +0800

    Update warp-xui-guard.sh

--- warp-xui-guard.sh
+++ warp-xui-guard.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 apt-get update -y && apt-get install curl -y
-
 apt install wget
 
 # 提示用户选择是否继续安装Warp
@@ -10,27 +9,23 @@ read -p "是否继续安装warp？ (y/n): " choice1
 if [ "$choice1" == "y" ]; then
     # 执行安装Warp的代码
     wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh
-
-
-else
-    echo "安装已取消。"
+    echo "Warp安装完成！"
 fi
 
- # 提示用户选择是否继续安装x-ui
-    read -p "Warp安装完成！是否继续安装x-ui？ (y/n): " choice2
+# 提示用户选择是否继续安装x-ui
+read -p "是否继续安装x-ui？ (y/n): " choice2
 
-    if [ "$choice2" == "y" ]; then
-        bash <(curl -Ls https://raw.githubusercontent.com/eljsz/personal_script/master/x-ui/install.sh)
+if [ "$choice2" == "y" ]; then
+    bash <(curl -Ls https://raw.githubusercontent.com/eljsz/personal_script/master/x-ui/install.sh)
+    echo "x-ui安装完成！"
+fi
 
-        # 提示用户选择是否继续安装x-ui+xray进程守护
-        read -p "x-ui安装完成！是否继续安装x-ui+xray进程守护，30秒检测一次，挂了自动运行？ (y/n): " choice3
-    
-        if [ "$choice3" == "y" ]; then
-            bash <(curl -Ls https://raw.githubusercontent.com/eljsz/personal_script/master/check_x-ui.sh)
-            echo "整合脚本安装完成！"
-        else
-            echo "安装已完成！"
-        fi
-    else
-        echo "安装已取消。"
-    fi
+# 提示用户选择是否继续安装x-ui+xray进程守护
+read -p "是否继续安装x-ui+xray进程守护，30秒检测一次，挂了自动运行？ (y/n): " choice3
+
+if [ "$choice3" == "y" ]; then
+    bash <(curl -Ls https://raw.githubusercontent.com/eljsz/personal_script/master/check_x-ui.sh)
+    echo "整合脚本安装完成！"
+else
+    echo "安装已完成！"
+fi
