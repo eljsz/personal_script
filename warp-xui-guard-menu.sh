629c248bf96804d1c4772f649108ad2cb974695d
eljsz <137702135+eljsz@users.noreply.github.com>
2023-08-12 21:35:38 +0800

    Update warp-xui-guard-menu.sh

--- warp-xui-guard-menu.sh
+++ warp-xui-guard-menu.sh
@@ -1,52 +1,43 @@
 #!/bin/bash
 
-# 定义菜单选项数组
-options=("安装 Warp"
-         "安装 x-ui"
-         "安装 x-ui + 进程守护"
-         "全部安装"
-         "退出")
+apt-get update -y && apt-get install curl -y
+apt-get install wget -y
 
-# 设置选择提示
-PS3="请输入选项号码（或按 Ctrl+C 退出）： "
+# 显示 dialog 菜单并获取用户选择
+choice=$(dialog --menu "选择安装选项" 12 50 5 \
+    1 "安装 Warp" \
+    2 "安装 x-ui" \
+    3 "安装 x-ui + 进程守护" \
+    4 "全部安装" \
+    5 "退出" 3>&1 1>&2 2>&3)
 
-# 显示菜单并等待用户选择
-select option in "${options[@]}"; do
-    case $option in
-        "安装 Warp")
-            # 执行安装 Warp 的代码
-            wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh
-            echo "Warp 安装完成！"
-            ;;
-        "安装 x-ui")
-            bash <(curl -Ls https://raw.githubusercontent.com/eljsz/script-integration/master/x-ui/install.sh)
-            echo "x-ui 安装完成！"
-            ;;
-        "安装 x-ui + 进程守护")
-            bash <(curl -Ls https://raw.githubusercontent.com/eljsz/script-integration/master/check_x-ui.sh)
-            echo "x-ui + 进程守护安装完成！"
-            ;;
-        "全部安装")
-            # 安装 Warp
-            wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh
-            echo "Warp 安装完成！"
-
-            # 安装 x-ui
-            bash <(curl -Ls https://raw.githubusercontent.com/eljsz/script-integration/master/x-ui/install.sh)
-            echo "x-ui 安装完成！"
-
-            # 安装 x-ui + 进程守护
-            bash <(curl -Ls https://raw.githubusercontent.com/eljsz/script-integration/master/check_x-ui.sh)
-            echo "x-ui + 进程守护安装完成！"
-            ;;
-        "退出")
-            echo "退出脚本"
-            exit 0
-            ;;
-        *)
-            echo "无效选项"
-            ;;
-    esac
-done
+case $choice in
+    1)
+        # 安装 Warp
+        wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh
+        echo "Warp 安装完成！"
+        ;;
+    2)
+        # 安装 x-ui
+        bash <(curl -Ls https://raw.githubusercontent.com/eljsz/script-integration/master/x-ui/install.sh)
+        echo "x-ui 安装完成！"
+        ;;
+    3)
+        # 安装 x-ui + 进程守护
+        bash <(curl -Ls https://raw.githubusercontent.com/eljsz/script-integration/master/check_x-ui.sh)
+        echo "x-ui + 进程守护安装完成！"
+        ;;
+    4)
+        # 安装全部
+        # ... 执行全部安装代码 ...
+        ;;
+    5)
+        echo "退出脚本"
+        exit 0
+        ;;
+    *)
+        echo "无效选项"
+        ;;
+esac
 
 echo "电报群：https://t.me/+vTwBHA9RbtMwYzA9"
