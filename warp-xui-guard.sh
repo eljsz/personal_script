155184c71e34ac6eb3d899ed667b1c9c8f281f9b
eljsz <137702135+eljsz@users.noreply.github.com>
2023-08-12 21:26:45 +0800

    Update warp-xui-guard.sh

--- warp-xui-guard.sh
+++ warp-xui-guard.sh
@@ -1,33 +1,45 @@
 #!/bin/bash
 
 apt-get update -y && apt-get install curl -y
-apt install wget
+apt-get install wget -y
 
-# 提示用户选择是否继续安装Warp
-read -p "是否继续安装warp？ (y/n): " choice1
+# 显示菜单并等待用户选择
+select option in "1 安装 Warp" "2 安装 x-ui" "3 安装 x-ui + 进程守护" "4 全部安装" "退出"; do
+    case $option in
+        "1")
+            # 执行安装 Warp 的代码
+            wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh
+            echo "Warp 安装完成！"
+            ;;
+        "2")
+            bash <(curl -Ls https://raw.githubusercontent.com/eljsz/script-integration/master/x-ui/install.sh)
+            echo "x-ui 安装完成！"
+            ;;
+        "3")
+            bash <(curl -Ls https://raw.githubusercontent.com/eljsz/script-integration/master/check_x-ui.sh)
+            echo "x-ui + 进程守护安装完成！"
+            ;;
+        "4")
+            # 安装 Warp
+            wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh
+            echo "Warp 安装完成！"
 
-if [ "$choice1" == "y" ]; then
-    # 执行安装Warp的代码
-    wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh
-    echo "Warp安装完成！"
-fi
+            # 安装 x-ui
+            bash <(curl -Ls https://raw.githubusercontent.com/eljsz/script-integration/master/x-ui/install.sh)
+            echo "x-ui 安装完成！"
 
-# 提示用户选择是否继续安装x-ui
-read -p "是否继续安装x-ui？ (y/n): " choice2
-
-if [ "$choice2" == "y" ]; then
-    bash <(curl -Ls https://raw.githubusercontent.com/eljsz/script-integration/master/x-ui/install.sh)
-    echo "x-ui安装完成！"
-	
-	# 提示用户选择是否继续安装x-ui+xray进程守护
-	read -p "是否继续安装x-ui+xray进程守护，30秒检测一次，挂了自动运行？ (y/n): " choice3
-
-	if [ "$choice3" == "y" ]; then
-		bash <(curl -Ls https://raw.githubusercontent.com/eljsz/script-integration/master/check_x-ui.sh)
-		echo "整合脚本安装完成！"
-	else
-		echo "安装已完成！"
-	fi
-fi
+            # 安装 x-ui + 进程守护
+            bash <(curl -Ls https://raw.githubusercontent.com/eljsz/script-integration/master/check_x-ui.sh)
+            echo "x-ui + 进程守护安装完成！"
+            ;;
+        "退出")
+            echo "退出脚本"
+            exit 0
+            ;;
+        *)
+            echo "无效选项"
+            ;;
+    esac
+done
 
 echo "电报群：https://t.me/+vTwBHA9RbtMwYzA9"
