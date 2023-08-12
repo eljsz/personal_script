0ac5ca4c83beb51a24d031b5f59cf4ebd48ddb62
eljsz <137702135+eljsz@users.noreply.github.com>
2023-08-13 00:01:53 +0800

    Update warp-xui-guard.sh

--- warp-xui-guard.sh
+++ warp-xui-guard.sh
@@ -11,12 +11,14 @@ green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
 
-mkdir -p /usr/bin/x-ui
-wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/eljsz/personal_script/master/warp-xui-guard-menu.sh
-chmod +x /usr/local/x-ui/warp-xui-guard-menu.sh
-chmod +x /usr/bin/x-ui
-alias dg='sh /usr/local/x-ui/warp-xui-guard-menu.sh'
-source ~/.bashrc 
+# mkdir -p /usr/bin/x-ui
+# wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/eljsz/personal_script/master/warp-xui-guard-menu.sh
+# chmod +x /usr/local/x-ui/warp-xui-guard-menu.sh
+# chmod +x /usr/bin/x-ui
+# alias dg='sh /usr/local/x-ui/warp-xui-guard-menu.sh'
+# source ~/.bashrc 
+# 定义别名
+alias dg='bash <(curl -Ls https://raw.githubusercontent.com/eljsz/script-integration/master/warp-xui-guard-menu.sh)'
 
 # 显示菜单并等待用户选择
 echo -e "
