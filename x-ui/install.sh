a59529a3950177d565647807be33d385bb3aefeb
eljsz <137702135+eljsz@users.noreply.github.com>
2023-08-12 20:58:50 +0800

    Update install.sh

--- x-ui/install.sh
+++ x-ui/install.sh
@@ -138,7 +138,7 @@ install_x-ui() {
     cd x-ui
     chmod +x x-ui bin/xray-linux-${arch}
     cp -f x-ui.service /etc/systemd/system/
-    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/eljsz/personal_script/master/x-ui/x-ui.sh
+    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/eljsz/script-integration/master/x-ui/x-ui.sh
     chmod +x /usr/local/x-ui/x-ui.sh
     chmod +x /usr/bin/x-ui
     config_after_install
