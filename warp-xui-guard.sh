a42ebc60784f7bdbd99b2e6d0d639ba4a86a7ce7
eljsz <137702135+eljsz@users.noreply.github.com>
2023-08-12 20:46:13 +0800

    Update warp-xui-guard.sh

--- warp-xui-guard.sh
+++ warp-xui-guard.sh
@@ -18,14 +18,15 @@ read -p "是否继续安装x-ui？ (y/n): " choice2
 if [ "$choice2" == "y" ]; then
     bash <(curl -Ls https://raw.githubusercontent.com/eljsz/personal_script/master/x-ui/install.sh)
     echo "x-ui安装完成！"
+	
+	# 提示用户选择是否继续安装x-ui+xray进程守护
+	read -p "是否继续安装x-ui+xray进程守护，30秒检测一次，挂了自动运行？ (y/n): " choice3
+
+	if [ "$choice3" == "y" ]; then
+		bash <(curl -Ls https://raw.githubusercontent.com/eljsz/personal_script/master/check_x-ui.sh)
+		echo "整合脚本安装完成！"
+	else
+		echo "安装已完成！"
+	fi
 fi
 
-# 提示用户选择是否继续安装x-ui+xray进程守护
-read -p "是否继续安装x-ui+xray进程守护，30秒检测一次，挂了自动运行？ (y/n): " choice3
-
-if [ "$choice3" == "y" ]; then
-    bash <(curl -Ls https://raw.githubusercontent.com/eljsz/personal_script/master/check_x-ui.sh)
-    echo "整合脚本安装完成！"
-else
-    echo "安装已完成！"
-fi
