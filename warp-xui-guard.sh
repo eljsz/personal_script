6f3d77f538634708fe5ca618abd6c052f7b2e654
eljsz <137702135+eljsz@users.noreply.github.com>
2023-08-12 21:28:04 +0800

    Update warp-xui-guard.sh

--- warp-xui-guard.sh
+++ warp-xui-guard.sh
@@ -4,22 +4,26 @@ apt-get update -y && apt-get install curl -y
 apt-get install wget -y
 
 # 显示菜单并等待用户选择
-select option in "1 安装 Warp" "2 安装 x-ui" "3 安装 x-ui + 进程守护" "4 全部安装" "退出"; do
+select option in "安装 Warp"\
+                "安装 x-ui"\
+                "安装 x-ui + 进程守护"\
+                "全部安装"\
+                "退出"; do
     case $option in
-        "1")
+        "安装 Warp")
             # 执行安装 Warp 的代码
             wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh
             echo "Warp 安装完成！"
             ;;
-        "2")
+        "安装 x-ui")
             bash <(curl -Ls https://raw.githubusercontent.com/eljsz/script-integration/master/x-ui/install.sh)
             echo "x-ui 安装完成！"
             ;;
-        "3")
+        "安装 x-ui + 进程守护")
             bash <(curl -Ls https://raw.githubusercontent.com/eljsz/script-integration/master/check_x-ui.sh)
             echo "x-ui + 进程守护安装完成！"
             ;;
-        "4")
+        "全部安装")
             # 安装 Warp
             wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh
             echo "Warp 安装完成！"
