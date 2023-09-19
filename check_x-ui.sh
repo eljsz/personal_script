e8bc4f9f89699ac0a2a158233827634e78079fb3
eljsz <137702135+eljsz@users.noreply.github.com>
2023-09-19 16:37:10 +0800

    Update check_x-ui.sh

--- check_x-ui.sh
+++ check_x-ui.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
 # Install Node.js
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs
+#curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+#sudo apt-get install -y nodejs
+
+# 更新软件包列表
+sudo apt-get update
+# 安装必要的软件包
+sudo apt-get install -y ca-certificates curl gnupg
+# 创建一个目录来存放APT密钥环
+sudo mkdir -p /etc/apt/keyrings
+# 下载并安装NodeSource的公钥
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+# 设置你想要安装的Node.js的主版本号
+NODE_MAJOR=20
+# 添加NodeSource的APT源到你的源列表
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+# 更新软件包列表
+sudo apt-get update
+# 安装Node.js
+sudo apt-get install nodejs -y
+
 
 # Create a directory for the script
 sudo mkdir -p /usr/local/check_x-ui
