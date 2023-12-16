4bcc56b0dde60c4f860ba64f6ee77b2df93b35b0
eljsz <137702135+eljsz@users.noreply.github.com>
2023-12-16 17:03:43 +0800

    Update install_py.sh

--- install_py.sh
+++ install_py.sh
@@ -23,6 +23,7 @@ fi
 echo "Installing requests module..."
 pip3 install requests
 pip3 install multidict
+pip3 install typing_extensions
 pip3 install aiohttp
 
 # Display a message indicating the installation is complete
