71ee231fac359cfcf07649563bed6c4ffba8cf7d
eljsz <137702135+eljsz@users.noreply.github.com>
2023-12-19 19:33:38 +0800

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -1,24 +1,15 @@
 #!/bin/sh
 
-# Update the package list
 sudo apt-get update
-
-# Install dependencies
 sudo apt-get install -y ca-certificates curl gnupg
-
-# Create a directory for the GPG key
 sudo mkdir -p /etc/apt/keyrings
-
-# Download the GPG key for the Node.js repository
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
-# Add the Node.js repository to the sources list
+
+NODE_MAJOR=18
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
-# Update the package list
 sudo apt-get update
-
-# Install Node.js
 sudo apt-get install nodejs -y
 
 # Install npm
